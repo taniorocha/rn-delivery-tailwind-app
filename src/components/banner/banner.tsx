c19aaa6c2596112dc4530542272836060ebcf401
hortaniorocha <hortanio@gmail.com>
2025-02-12 17:23:30 -0300

    Fixing project structure and first page

---
+++ src/components/banner/banner.tsx
@@ -0,0 +1,21 @@
+import { View, Pressable, Text, Image } from "react-native";
+import PagerView from 'react-native-pager-view';
+
+export function Banner() {
+    return (
+        <View className="w-full h-36 rounded-2xl mt-5 mb-4">
+            <PagerView>
+                {/* <Pressable 
+                    className="w-full h-36 rounded-2xl" 
+                    key="1"
+                    onPress={() => console.log("Clicou no banner 1")}
+                >
+                    <Image 
+                        source={require("../../assets/banner1.png")}
+                        className="w-full h-36 rounded-2xl"
+                    ></Image>
+                </Pressable> */}
+            </PagerView>
+        </View>
+    );
+}
